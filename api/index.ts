d478e688ef0860b640520b067ca0bf757444d0a2
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-17 11:25:49 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -7,7 +7,15 @@ let number = 0;
 
 app.use(express.json());
 
+interface data {
+  qn: string
+}
 
+function cs(input: data) void {
+  const object = Object.keys(input);
+const keys = object.map(key => `"${object}"`).join(', ');
+return keys
+}
 app.get('/api/calc', (req: Request, res: Response) => {
   const plus = parseInt(req.query.plus as string, 10);
   const minus = parseInt(req.query.minus as string, 10);
@@ -23,14 +31,42 @@ const divided = parseInt(req.query.divided as string, 10);
 
     number += plus || 0;
     number -= minus || 0;
-
+    number *= times || 0;
+    number /= times || 0;
+    
+if (cs(plus) == "plus") {
     res.json([{
       status: 'success',
-      message: 'Number Modified',
+      message: 'Number Increased',
       data: {
         number: number,
       }
     }]);
+} else if (cs(minus) == "minus") {
+  res.json([{
+      status: 'success',
+      message: 'Number Decreased',
+      data: {
+        number: number,
+      }
+    }]);
+} else if (cs(times) == "times") {
+  res.json([{
+      status: 'success',
+      message: 'Number Times',
+      data: {
+        number: number,
+      }
+    }]);
+} else if (cs(divided) == "divided") {
+  res.json([{
+      status: 'success',
+      message: 'Number Divided',
+      data: {
+        number: number,
+      }
+    }]);
+}
   } catch (error) {
     if (error.message === 'Invalid Parameters') {
       return res.status(400).json([{
