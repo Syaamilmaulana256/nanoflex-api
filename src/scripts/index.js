8f922def5ab4bfc37f0229cbdfb8a159bbb309bb
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-22 13:23:34 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
 document.addEventListener("DOMContentLoaded", function() {
     const ctaBtn = document.querySelector(".cta-btn");
     
