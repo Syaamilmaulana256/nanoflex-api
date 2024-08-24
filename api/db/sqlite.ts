5f59b6af8398012f30c37be7d85a70191feac854
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-24 12:51:18 +0700

    Update sqlite.ts

--- api/db/sqlite.ts
+++ api/db/sqlite.ts
@@ -2,7 +2,7 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
 export const dbPromise = open({
-  filename: './filelinker.db',
+  filename: './data.db',
   driver: sqlite3.Database
 });
 
