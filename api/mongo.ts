f4ec170ac073a2fc7172ff119955a0a2d6da2245
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-30 20:07:50 +0700

    Update mongo.ts

--- api/mongo.ts
+++ api/mongo.ts
@@ -1,15 +1,25 @@
-// db/mongo.ts
 import { MongoClient, Db } from 'mongodb';
 
-const uri = "mongodb+srv://syaamilmaulana2755:klhYSiYsaWOMZh7v@cluster0.c3icm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://syaamilmaulana2755:klhYSiYsaWOMZh7v@cluster0.c3icm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";";
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
 
-let db: Db;
+if (!uri) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
 
 export async function connectToDatabase() {
-  if (!db) {
-    await client.connect();
-    db = client.db("aura-apidb");
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
   }
-  return db;
+
+  const client = new MongoClient(uri!);
+
+  await client.connect();
+  const db = client.db("aura-apidb");
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
 }
