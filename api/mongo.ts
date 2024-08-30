95f673d201a3c1dea54efa657296557df2364020
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-30 20:17:08 +0700

    Update mongo.ts

--- api/mongo.ts
+++ api/mongo.ts
@@ -1,6 +1,8 @@
 import { MongoClient, Db } from 'mongodb';
 
+// Masukkan URI MongoDB Anda di sini.
 const uri = "mongodb+srv://syaamilmaulana2755:klhYSiYsaWOMZh7v@cluster0.c3icm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 let cachedClient: MongoClient | null = null;
 let cachedDb: Db | null = null;
 
@@ -9,17 +11,24 @@ if (!uri) {
 }
 
 export async function connectToDatabase() {
+  // Jika koneksi sudah ada, gunakan cache untuk menghindari pembuatan koneksi baru setiap saat.
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb };
   }
 
-  const client = new MongoClient(uri!);
+  // Buat instance baru MongoClient
+  const client = new MongoClient(uri);
 
+  // Tunggu hingga koneksi berhasil
   await client.connect();
+
+  // Akses database berdasarkan nama
   const db = client.db("aura-apidb");
 
+  // Cache client dan db agar tidak perlu reconnect setiap saat
   cachedClient = client;
   cachedDb = db;
 
+  // Kembalikan client dan db
   return { client, db };
 }
