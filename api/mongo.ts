f1c90801a042acb8a5a19f1f14c56e8c6c13c59f
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-30 19:25:57 +0700

    Update mongo.ts

--- api/mongo.ts
+++ api/mongo.ts
@@ -1,15 +1,15 @@
 // db/mongo.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://syaamilmaulana2755:klhYSiYsaWOMZh7v@cluster0.c3icm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
-let db;
+let db: Db;
 
 export async function connectToDatabase() {
   if (!db) {
     await client.connect();
-    db = client.db(process.env.DB_NAME);
+    db = client.db("aura-apidb");
   }
   return db;
 }
