982ab996954a622006fe3e2463a1d6c32b2d6ef4
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-08-30 19:16:43 +0700

    Update mongo.ts

--- api/mongo.ts
+++ api/mongo.ts
@@ -1,7 +1,7 @@
 // db/mongo.ts
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 let db;
@@ -9,7 +9,7 @@ let db;
 export async function connectToDatabase() {
   if (!db) {
     await client.connect();
-    db = client.db(process.env.DB_NAME || 'mydatabase');
+    db = client.db(process.env.DB_NAME);
   }
   return db;
 }
