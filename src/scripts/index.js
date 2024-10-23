722c8d8628aea5921b947644da1c41b59e530a7d
Syaamil Maulana <106651762+Syaamilmaulana256@users.noreply.github.com>
2024-10-23 21:34:50 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(response => {
       if (!response.ok) {
-        throw new Error(`Error : ${response.status}`);
+        throw new Error(`${response.status}`);
       }
       return response.json();
     })
